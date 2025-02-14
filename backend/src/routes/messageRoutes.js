fd5fdf5ac9f2be7d7e69f4ddab812e7023a015fe
Fahmi Azzuhri Efki <fahmiazzuhriefki@gmail.com>
2025-02-14 08:51:58 +0700

    integrate fe and be

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -4,7 +4,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", sendMessage);
+router.post("/sendMessage", sendMessage);
 router.get("/getAllMessage", authMiddleware, getMessages);
 
 export default router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,30 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+
 const App = () => {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [content, setContent] = useState("");
+  const sendMessageMutation = useMutation({
+    mutationFn: async ({ from, to, message }) => {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_ENDPOINT}/api/messages/sendMessage`,
+        { from, to, content }
+      );
+      return response.data;
+    },
+    onSuccess: () => {
+      window.open("berhasil");
+    },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    sendMessageMutation.mutate({ from, to, content });
+  };
+
+  const isValid = from && to && content;
   return (
     <div className="flex min-h-screen items-center justify-center p-6">
       <div className="grid md:grid-cols-2 gap-6 max-w-4xl rounded-lg p-8 shadow-lg">
@@ -10,22 +36,32 @@ const App = () => {
             "Speak freely, stay anonymous. Share your thoughts, and express
             yourself without fear."
           </h2>
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit} className="space-y-4">
             <input
               type="text"
+              onChange={(e) => setFrom(e.target.value)}
+              value={from}
               placeholder="From"
               className="w-full border-b-2 focus:outline-none focus:border-pink-500 p-2"
             />
             <input
               type="text"
+              onChange={(e) => setTo(e.target.value)}
+              value={to}
               placeholder="To"
               className="w-full border-b-2 focus:outline-none focus:border-pink-500 p-2"
             />
             <textarea
               placeholder="Your message"
+              onChange={(e) => setContent(e.target.value)}
+              value={content}
               className="w-full border-b-2 focus:outline-none focus:border-pink-500 p-2"
             ></textarea>
-            <button className="w-full bg-pink-500 text-white py-2 rounded-lg flex items-center justify-center gap-2 cursor-pointer hover:bg-pink-600">
+            <button
+              type="submit"
+              disabled={!isValid}
+              className="w-full bg-pink-500 text-white py-2 rounded-lg flex items-center justify-center gap-2 cursor-pointer hover:bg-pink-600"
+            >
               ✈ Send Message
             </button>
           </form>
