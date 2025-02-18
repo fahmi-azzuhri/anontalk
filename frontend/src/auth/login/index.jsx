ceb3e2ae3817a3969c90a30d883c82f7b9a88147
Fahmi Azzuhri Efki <fahmiazzuhriefki@gmail.com>
2025-02-18 14:32:38 +0700

    integrate login

--- frontend/src/auth/login/index.jsx
+++ frontend/src/auth/login/index.jsx
@@ -1,6 +1,45 @@
-import React from "react";
-
+import { useMutation } from "@tanstack/react-query";
+import React, { useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const isValid = email && password;
+  const navigate = useNavigate();
+  const loginMutation = useMutation({
+    mutationFn: async ({ email, password }) => {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_ENDPOINT}/api/auth/login-admin`,
+        {
+          email,
+          password,
+        }
+      );
+      return response.data;
+    },
+    onSuccess: (data) => {
+      const { token } = data;
+      Cookies.set("token", token, { expires: 1 });
+      setTimeout(() => {
+        navigate("/auth/onlyadmin/dashboard");
+      }, 2500);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+  const handleLogin = () => {
+    e.preventDefault();
+    if (isValid) {
+      toast.promise(loginMutation.mutateAsync({ email, password }), {
+        loading: "Please wait",
+        success: "Login successful",
+        error: "Login failed, password or email is incorrect",
+      });
+    }
+  };
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
@@ -14,19 +53,26 @@ function Login() {
         <div className="space-y-4">
           <input
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             className="w-full px-4 py-2 border border-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             className="w-full px-4 py-2 bg-gray-100 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <div className="text-right text-blue-600 text-sm cursor-pointer">
             Forgot your password?
           </div>
-          <button className="w-full py-2 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700">
-            Sign in
+          <button
+            onClick={handleLogin}
+            className="w-full py-2 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700"
+          >
+            Login
           </button>
         </div>
       </div>
