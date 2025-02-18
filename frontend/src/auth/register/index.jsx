5e99ad373356a1e899b6c58036f937e9ca04b4e1
Fahmi Azzuhri Efki <fahmiazzuhriefki@gmail.com>
2025-02-18 10:52:35 +0700

    add logic register

--- frontend/src/auth/register/index.jsx
+++ frontend/src/auth/register/index.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate();
+  const matchPassword = () => {
+    if (!password || !confirmPassword) {
+      toast.error("Please fill all the fields");
+      return;
+    }
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+    toast.success("Account created successfully!");
+    setTimeout(() => {
+      navigate("/auth/onlyadmin/login-admin");
+    }, 2500);
+  };
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
         <h2 className="text-2xl font-bold text-center text-blue-600">
           Create Account
@@ -16,24 +35,33 @@ function Register() {
         <div className="space-y-4">
           <input
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             className="w-full px-4 py-2 border border-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             className="w-full px-4 py-2 bg-gray-100 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             placeholder="Confirm Password"
             className="w-full px-4 py-2 bg-gray-100 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <button className="w-full py-2 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700">
-            Sign up
+          <button
+            onClick={matchPassword}
+            className="w-full py-2 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700"
+          >
+            Register
           </button>
           <p className="text-center text-gray-600">
-            Already have an account?{" "}
+            Already have an account? {""}
             <button
               onClick={() => navigate("/auth/onlyadmin/login-admin")}
               className="text-blue-600 font-semibold cursor-pointer"
